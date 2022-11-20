430aacb64c7978d1068f9502d012dbf34410dee1
ziyaoxu <52606686+ziyaoxu@users.noreply.github.com>
2022-11-19 22:26:05 -0500

    Update readme.m

--- Triangular mesh/Example 5.1 Complex/readme.m
+++ Triangular mesh/Example 5.1 Complex/readme.m
@@ -1,3 +1,6 @@
-% Example 5.4 of the paper 
-% The hybrid-dimensional Darcy's law: a non-conforming reinterpreted discrete fracture model (RDFM) for single-phase flow in fractured media,
-% Ziyao Xu, Zhaoqin Huang, Yang Yang
+% It is the Example 5.4 of our paper.
+
+% taken from benchmarks of the paper: 
+% "Benchmarks for single-phase flow in fractured porous media." 
+% Advances in Water Resources 111 (2018): 239-258.
+% Flemisch, Bernd, Inga Berre, Wietse Boon, Alessio Fumagalli, Nicolas Schwenck, Anna Scotti, Ivar Stefansson, and Alexandru Tatomir. 
